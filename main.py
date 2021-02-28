aee2d94a9afa5752178b9d2302d7ba3722a2e90b
Harsh <harsh.26020@gmail.com>
2021-02-28 10:06:33 +0530

    Fixed error in read input

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ def read_input():
 	payoffs = np.array(list(map(int,input().strip().split())))
 	n_size = num_strategy
 	n_size = np.append(n_size, num_players)
-	# payoffs i,j,k,.....,X = payoff for player X for s1i, s2j, s3k,....
 	payoffs.resize(n_size)
+	# payoffs X,i,j,k,.... = payoff for player X for s1i, s2j, s3k,....
+	payoffs = np.transpose(payoffs)
 	return num_players, num_strategy, payoffs
 
 if __name__ == '__main__':
-	read_input()
-	# num_players, num_strategy, payoffs = read_input()
+	num_players, num_strategy, payoffs = read_input()
